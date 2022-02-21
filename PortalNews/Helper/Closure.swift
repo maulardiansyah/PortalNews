1f030fe02bd76b07f327c134dd7a94e51ac9be8a
Maulana Ardiansyah <maul.ardiansyahh@gmail.com>
2022-02-21 15:38:11 +0700

    added closure base

---
+++ PortalNews/Helper/Closure.swift
@@ -0,0 +1,9 @@
+//
+//  Closure.swift
+//  PortalNews
+//
+//  Created by Maul on 21/02/22.
+//
+
+public typealias SelectionClosure = () -> Void
+public typealias SelectionStringClosure = (String) -> Void
